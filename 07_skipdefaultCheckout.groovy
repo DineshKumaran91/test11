a30f551c6970ce82db040ac299b930c4ea55a633
Dinesh Kumaran <d,kumaran.m@gmail.com>
2022-04-19 13:28:47 +0530

    first commit

---
+++ 07_skipdefaultCheckout.groovy
@@ -0,0 +1,17 @@
+// chooses any agent
+pipeline{
+    agent none
+    stages{
+        stage("Test"){
+            agent any
+            options {
+                skipDefaultCheckout()
+            }
+            steps{
+                echo "Hello world"
+            }
+        }
+    }
+}
+
+https://github.com/DineshKumaran91/day_01.git
